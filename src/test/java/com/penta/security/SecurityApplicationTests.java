2c1fd1451a1b5cf71a090c5aa8d6f3d065f3e66c
Park JeaHyun <jea5158@gmail.com>
2024-11-25 21:51:23 +0900

    회원 삽입 기능 테스트 코드 작성

--- src/test/java/com/penta/security/SecurityApplicationTests.java
+++ src/test/java/com/penta/security/SecurityApplicationTests.java
@@ -1,13 +1,38 @@
 package com.penta.security;
 
+import com.penta.security.user.dto.request.SystemUserCreateRequestDto;
+import com.penta.security.user.dto.response.SystemUserResponseDto;
+import com.penta.security.user.service.SystemUserService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class SecurityApplicationTests {
 
+	@Autowired
+	private SystemUserService systemUserService;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("회원 삽입 기능 테스트")
+	void systemUserCreateTest() {
+		// given
+		SystemUserCreateRequestDto requestDto = new SystemUserCreateRequestDto();
+		requestDto.setUserId("test_user_id");
+		requestDto.setUserPw("test_user_pw");
+		requestDto.setUserNm("test_user_nm");
+
+		// when
+		SystemUserResponseDto responseDto = systemUserService.create(requestDto);
+
+		// then
+		String actualUserAuth = responseDto.getUserAuth();
+		assertThat(actualUserAuth).isEqualTo("SYSTEM_USER");
 	}
 
 }
